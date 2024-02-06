c74dcd748c240899e5f01be5773705faa28350ba
k.takaya <k.takaya@marukinad.co.jp>
2024-02-06 13:10:24 +0900

    maru-school

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from "astro/config";
-import glsl from "vite-plugin-glsl";
-import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
 
 const baseDir = "/maru_school/takaya/transition/";
@@ -9,6 +6,5 @@ const baseDir = "/maru_school/takaya/transition/";
 export default defineConfig({
   base: baseDir,
   outDir: `./dist${baseDir}`,
-  integrations: [react(), tailwind()],
-  plugins: [glsl()],
+  integrations: [tailwind()],
 });
---
+++ src/components/posts/Posts.ts
@@ -0,0 +1,37 @@
+export const items = [
+  {
+    link: "about",
+    title: "About",
+    description: "こちらは About Page へのリンクです",
+  },
+  {
+    link: "works",
+    title: "Works",
+    description: "こちらは Works Page へのリンクです",
+  },
+  {
+    link: "contact",
+    title: "Contact",
+    description: "こちらは Contact Page へのリンクです",
+  },
+  {
+    link: "test",
+    title: "Test",
+    description: "こちらは Test Page へのリンクです",
+  },
+  {
+    link: "test2",
+    title: "Test2",
+    description: "こちらは Test2 Page へのリンクです",
+  },
+  {
+    link: "test3",
+    title: "Test3",
+    description: "こちらは Test3 Page へのリンクです",
+  },
+  {
+    link: "test4",
+    title: "Test4",
+    description: "こちらは Test4 Page へのリンクです",
+  },
+];
