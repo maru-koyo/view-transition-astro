18ccbcf129326b6782aa252b23de599a13ee65b2
k.takaya <k.takaya@marukinad.co.jp>
2024-02-06 18:10:47 +0900

    astroテンプレート

---
+++ src/components/scroller/Scroller.ts
@@ -0,0 +1,19 @@
+import Lenis from "@studio-freight/lenis";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function Scroller(): void {
+  const lenis = new Lenis();
+
+  lenis.on("scroll", (e: Event) => {});
+
+  lenis.on("scroll", ScrollTrigger.update);
+
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+
+  gsap.ticker.lagSmoothing(0);
+}
