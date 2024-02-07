8c1f4f1a1d71b02fc5664e0d14f1dd7e3a541a83
k.takaya <k.takaya@marukinad.co.jp>
2024-02-07 12:52:44 +0900

    b

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from "astro/config";
-import glslify from "rollup-plugin-glslify";
 import tailwind from "@astrojs/tailwind";
 const baseDir = "/maru_school/takaya/transition/";
 
@@ -8,38 +7,4 @@ export default defineConfig({
   base: baseDir,
   outDir: `./dist${baseDir}`,
   integrations: [tailwind()],
-  plugins: [
-    glslify({
-      compress(code) {
-        let needNewline = false;
-        return code
-          .replace(
-            /\\(?:\r\n|\n\r|\n|\r)|\/\*.*?\*\/|\/\/(?:\\(?:\r\n|\n\r|\n|\r)|[^\n\r])*/gs,
-            ""
-          )
-          .split(/\n+/)
-          .reduce((result, line) => {
-            line = line.trim().replace(/\s{2,}|\t/, " ");
-            if (line.charAt(0) === "#" || /else/.test(line)) {
-              if (needNewline) {
-                result.push("\n");
-              }
-              result.push(line, "\n");
-              needNewline = false;
-            } else {
-              result.push(
-                line.replace(
-                  /\s*({|}|=|\*|,|\+|\/|>|<|&|\||\[|\]|\(|\)|-|!|;)\s*/g,
-                  "$1"
-                )
-              );
-              needNewline = true;
-            }
-            return result;
-          }, [])
-          .join(process.env.NODE_ENV === "development" ? "\n" : "")
-          .replace(/\n+/g, "\n");
-      },
-    }),
-  ],
 });
